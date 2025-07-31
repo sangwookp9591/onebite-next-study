40194805e935a62d88dc683d221e8cc4a1569e0a
sangwookpark <sangwookp9591@gmail.com>
2025-07-31 17:28:13 +0900

    chore  : reactStrictMode false로 적용

--- section02/next.config.mjs
+++ section02/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: false, // true면 react app에서 잠재적인 문제를 검사하기위해 개발모드로 실행시켰을때 컴포넌트를 두번이나 실행시킴
 };
 
 export default nextConfig;
