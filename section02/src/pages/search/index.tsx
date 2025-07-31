e6111a7479be5f10d9efe854610e3bae0a27f7d0
sangwookpark <sangwookp9591@gmail.com>
2025-07-31 18:29:14 +0900

    feat : page router에서 query string

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,3 +1,16 @@
+import { useRouter } from 'next/router'; //navigation은 app router에서 사용
+
 export default function Page() {
-    return <h1>search</h1>;
+    //?q=asdasd
+    //query string 가져오는 방법
+
+    const router = useRouter();
+    console.log('router : ', router);
+
+    const { q } = router.query;
+
+    //search?q=asdasd
+    // 이렇게했을때 Log가 두번찍히는 이유는 querystring 읽는과정중 component를 한번더 렌더링시키기때문
+
+    return <h1>search {q}</h1>;
 }
