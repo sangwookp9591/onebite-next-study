a5da178c5266ffa5e1eb599160870c129cf8f804
sangwookpark <sangwookp9591@gmail.com>
2025-07-31 17:08:32 +0900

    refactor: standardize import quotes and improve formatting in _app.tsx and _document.tsx

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+//페이지의 역할을 하진않지만 공통 로직 및 레이아웃을 다루는 페이지
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
--- section02/src/pages/_document.tsx
+++ section02/src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
+//페이지의 역할을 하진않지만 공통 로직 및 레이아웃을 다루는 페이지
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
