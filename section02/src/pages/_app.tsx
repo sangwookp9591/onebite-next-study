dc254ce533e439c2c7f7614b003a408b8b403f64
sangwookpark <sangwookp9591@gmail.com>
2025-08-01 00:24:48 +0900

    fix : Link 컴포넌트가 아니면 pre-fetching이 이루어지지 않는다. 프로그래매틱도 가능하게 하려면 마운트될때(화면의 처음 그려질때) 직접 하면됨

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 //페이지의 역할을 하진않지만 공통 로직 및 레이아웃을 다루는 페이지
 
 //root component 모든 페이지를 하는 컴포넌트의 부모 컴포넌트
@@ -21,6 +22,10 @@ export default function App({ Component, pageProps }: AppProps) {
     const onClickButton = () => {
         router.push('/test');
     };
+
+    useEffect(() => {
+        router.prefetch('/test');
+    }, []);
     return (
         <>
             <header>
