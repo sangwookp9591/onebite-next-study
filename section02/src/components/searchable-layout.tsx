cae4775bfaf3b353a201a8e177936187a57f35a3
sangwookpark <sangwookp9591@gmail.com>
2025-08-01 20:14:31 +0900

    feat : query string값이 있을경우 searchbar에 반영

--- section02/src/components/searchable-layout.tsx
+++ section02/src/components/searchable-layout.tsx
@@ -1,9 +1,17 @@
 import { useRouter } from 'next/router';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 export default function SearchableLayout({ children }: { children: ReactNode }) {
     const router = useRouter();
     const [search, setSearch] = useState<string>('');
+
+    //최초이렇게 하면 queystring이 여러 개일 수도 있기 때문에 , string, String arr이나, undefined가 올 수 도있기때문임
+    // const q = router.query.q;
+    const q = router.query.q as string;
+
+    useEffect(() => {
+        setSearch(q || '');
+    }, [q]);
 
     const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target?.value);
