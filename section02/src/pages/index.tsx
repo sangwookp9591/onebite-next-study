ac4554129ba378ccc4e355e9c8b37fdf88355f80
sangwookpark <sangwookp9591@gmail.com>
2025-08-01 18:42:08 +0900

    feat : getLayout method로 searchLayout을 감싸서 Page return

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import SearchableLayout from '@/components/searchable-layout';
 import styles from './index.module.css';
 
 export default function Home() {
@@ -7,3 +8,9 @@ export default function Home() {
         </>
     );
 }
+
+//Home은 함수인데 ? 어떻게 함수에다가 getLayout method를 추가할수있는거지?
+//  -> js의 모든함수는 객체이기 때문임.
+Home.getLayout = (page: React.ReactNode) => {
+    return <SearchableLayout>{page}</SearchableLayout>;
+};
