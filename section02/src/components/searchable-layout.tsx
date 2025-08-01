dfb9cb41e914be7aa33ee6e481d15a42e6cfa4ae
sangwookpark <sangwookp9591@gmail.com>
2025-08-01 20:10:36 +0900

    feat : searchLayout input event hanlder 추가

--- section02/src/components/searchable-layout.tsx
+++ section02/src/components/searchable-layout.tsx
@@ -1,9 +1,19 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 
 export default function SearchableLayout({ children }: { children: ReactNode }) {
+    const [search, setSearch] = useState<string>('');
+
+    const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target?.value);
+    };
+
+    const onSubmit = () => {};
     return (
         <div>
-            <div>임시 서버</div>
+            <div>
+                <input type="text" placeholder="검색어를 입력하세요 ..." value={search} onChange={onChangeSearch} />
+                <button onClick={onSubmit}>검색</button>
+            </div>
             {children}
         </div>
     );
