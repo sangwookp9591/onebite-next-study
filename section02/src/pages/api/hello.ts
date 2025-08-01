859ea9b8766626a6365bb5c27d254caad90a3f51
sangwookpark <sangwookp9591@gmail.com>
2025-08-01 15:20:59 +0900

    docs : api routes

--- section02/src/pages/api/hello.ts
+++ section02/src/pages/api/hello.ts
@@ -1,13 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string;
+    name: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  res.status(200).json({ name: "John Doe" });
+// /api/hello 라는 경로를 갖게됨
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    res.status(200).json({ name: 'John Doe' });
 }
