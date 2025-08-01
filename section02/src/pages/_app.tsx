dd404469f8f75fe03bf02ba3860795408e1b5d8a
sangwookpark <sangwookp9591@gmail.com>
2025-08-01 19:38:18 +0900

    fix :  getLayout이 undefined 이면 page를 그대로 리턴하도록 수정

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import GlobalLayout from '@/components/global-layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
     console.log('Component getLayout', Component?.getLayout);
 
-    const getLayout = Component.getLayout;
+    const getLayout = Component.getLayout ?? ((page: React.ReactNode) => page); //undifined를 함수처럼 호출하려고하니깐 에러
 
     return (
         <>
