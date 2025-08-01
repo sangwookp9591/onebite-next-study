02703f788d6224ef65b12bbb15ce855e89c63f4a
sangwookpark <sangwookp9591@gmail.com>
2025-08-01 19:43:17 +0900

    fix : type error를 수정하기위해 AppProps의 타입 확장

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import GlobalLayout from '@/components/global-layout';
 import '@/styles/globals.css';
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import React from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+    getLayout?: (page: React.ReactNode) => React.ReactNode;
+};
+//  getLayout?는 없는 페이지가 있을 수도 있으니깐
+export default function App({
+    Component,
+    pageProps,
+}: AppProps & {
+    Component: NextPageWithLayout;
+}) {
     console.log('Component getLayout', Component?.getLayout);
 
     const getLayout = Component.getLayout ?? ((page: React.ReactNode) => page); //undifined를 함수처럼 호출하려고하니깐 에러
