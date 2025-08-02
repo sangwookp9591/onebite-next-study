edaa5cc901a6ee533817b4cb4c80a52de7613b6d
sangwookpark <sangwookp9591@gmail.com>
2025-08-02 21:46:32 +0900

    feat: container style및 구조 수정

--- section02/src/components/book-item.tsx
+++ section02/src/components/book-item.tsx
@@ -1,5 +1,19 @@
 import { BookData } from '@/types';
+import Link from 'next/link';
+import styles from './book-item.module.css';
 
 export default function BookItem({ id, title, subTitle, description, author, publisher, coverImgUrl }: BookData) {
-    return <div>도서 아이템</div>;
+    return (
+        <Link href={`/book/${id}`} className={styles.container}>
+            <img src={coverImgUrl} />
+            <div>
+                <div>{title}</div>
+                <div>{subTitle}</div>
+                <br />
+                <div>
+                    {author} | {publisher}
+                </div>
+            </div>
+        </Link>
+    );
 }
