1d24e662b41bac70b998be1b2d44a0208b2440d8
sangwookpark <sangwookp9591@gmail.com>
2025-08-02 21:54:57 +0900

    feat : title, subTitle, author style 추가

--- section02/src/components/book-item.tsx
+++ section02/src/components/book-item.tsx
@@ -7,10 +7,10 @@ export default function BookItem({ id, title, subTitle, description, author, pub
         <Link href={`/book/${id}`} className={styles.container}>
             <img src={coverImgUrl} />
             <div>
-                <div>{title}</div>
-                <div>{subTitle}</div>
+                <div className={styles.title}>{title}</div>
+                <div className={styles.subTitle}>{subTitle}</div>
                 <br />
-                <div>
+                <div className={styles.author}>
                     {author} | {publisher}
                 </div>
             </div>
