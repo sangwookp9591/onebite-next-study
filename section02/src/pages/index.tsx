4e616a663332d8dfe80aa07a03659f89caf4c966
sangwookpark <sangwookp9591@gmail.com>
2025-08-02 22:55:55 +0900

    feat : page bookitem 추가

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,22 +1,12 @@
 import SearchableLayout from '@/components/searchable-layout';
-import styles from './index.module.css';
 import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
 export default function Home() {
     return (
-        <div className={styles.container}>
-            <section>
-                <h3>지금 추천하는 도서</h3>
-                {books.map((book) => (
-                    <BookItem key={book?.id} {...book} />
-                ))}
-            </section>
-            <section>
-                <h3>등록된 모든 도서 </h3>
-                {books.map((book) => (
-                    <BookItem key={book?.id} {...book} />
-                ))}
-            </section>
+        <div>
+            {books.map((book) => {
+                return <BookItem key={book?.id} {...book} />;
+            })}
         </div>
     );
 }
