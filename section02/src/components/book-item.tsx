b5df4cbd9821f41925d356c67941aa2193af3a36
sangwookpark <sangwookp9591@gmail.com>
2025-08-02 21:18:16 +0900

    feat :BookItem에 BookData추가

--- section02/src/components/book-item.tsx
+++ section02/src/components/book-item.tsx
@@ -1,3 +1,5 @@
-export default function BookItem() {
+import { BookData } from '@/types';
+
+export default function BookItem({ id, title, subTitle, description, author, publisher, coverImgUrl }: BookData) {
     return <div>도서 아이템</div>;
 }
