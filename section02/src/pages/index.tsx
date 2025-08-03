41a444533e74dd2afd5f1ae29d13ce2e96d90b3e
sangwookpark <sangwookp9591@gmail.com>
2025-08-03 20:17:45 +0900

    feat : SSR로 설정 getServerSideProps=()=>{}

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -3,6 +3,11 @@ import styles from './index.module.css';
 import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
 
+/**  이렇게하면 이 index.tsx페이지는 ssr방식으로 사전렌더링이 이루어진다.
+왜그런가? getServerSideProps 라는 약속된 이름의 함수를 만들어서
+ export 하게되면 해당페이지는 SSR로 동작하도록 자동으로 설정됨
+ */
+export const getServerSideProps = () => {};
 export default function Home() {
     return (
         <div className={styles.container}>
