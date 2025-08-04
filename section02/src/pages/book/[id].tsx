18610e71b13fa91966437d8dff0d2e344ca202a3
sangwookpark <sangwookp9591@gmail.com>
2025-08-04 16:07:20 +0900

    feat : 다니나믹 book page에 getServerSideProps추가 및 book 하나만 조회하는 api추가

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,25 +1,27 @@
-import { useRouter } from 'next/router';
 import styles from './[id].module.css';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import fetchOneBooks from '@/lib/fetch-one-book';
 
-const mockData = {
-    id: 1,
-    title: '한 입 크기로 잘라 먹는 리액트',
-    subTitle: '자바스크립트 기초부터 애플리케이션 배포까지',
-    description:
-        '자바스크립트 기초부터 애플리케이션 배포까지\n처음 시작하기 딱 좋은 리액트 입문서\n\n이 책은 웹 개발에서 가장 많이 사용하는 프레임워크인 리액트 사용 방법을 소개합니다. 인프런, 유데미에서 5000여 명이 수강한 베스트 강좌를 책으로 엮었습니다. 프런트엔드 개발을 희망하는 사람들을 위해 리액트의 기본을 익히고 다양한 앱을 구현하는 데 부족함이 없도록 만들었습니다. \n\n자바스크립트 기초 지식이 부족해 리액트 공부를 망설이는 분, 프런트엔드 개발을 희망하는 취준생으로 리액트가 처음인 분, 퍼블리셔나 백엔드에서 프런트엔드로 직군 전환을 꾀하거나 업무상 리액트가 필요한 분, 뷰, 스벨트 등 다른 프레임워크를 쓰고 있는데, 실용적인 리액트를 배우고 싶은 분, 신입 개발자이지만 자바스크립트나 리액트 기초가 부족한 분에게 유용할 것입니다.',
-    author: '이정환',
-    publisher: '프로그래밍인사이트',
-    coverImgUrl: 'https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg',
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+    //url parameter를 불러오기윟
+    const id = context.params!.id;
+    //type에러를 막기위해 무조건 !.id 라는id값이 있을거다라는 형식을 쓴이유는
+    //이페이지 자체가 [id]로 되어있어 id가 있어야지만 접근이 가능하기 때문이다.
+
+    const book = await fetchOneBooks(Number(id)); // 기본적으로 String타입을 가짐
+    return { props: { book } };
 };
 
-export default function Page() {
-    const router = useRouter();
+export default function Page({ book }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    if (!book) return '문제가 발생했습니다 다시 실행해주세요.';
 
-    console.log('router : ', router);
+    // const router = useRouter();
 
-    const { id } = router.query;
+    // console.log('router : ', router);
 
-    const { title, subTitle, description, author, publisher, coverImgUrl } = mockData;
+    // const { id } = router.query;
+
+    const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
     return (
         <div className={styles.container}>
