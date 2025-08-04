81c4ff2d216a09d57f0e53eda1affa87d4fc0170
sangwookpark <sangwookp9591@gmail.com>
2025-08-04 16:06:38 +0900

    feat : url parameter로 book 하나만 가지고 오는 api 추가

---
+++ section02/src/lib/fetch-one-book.ts
@@ -0,0 +1,16 @@
+import { BookData } from '@/types';
+
+export default async function fetchOneBooks(id: number): Promise<BookData | null> {
+    const url = `http://localhost:12345/book/${id}`;
+
+    try {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error();
+        }
+        return await res.json();
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
