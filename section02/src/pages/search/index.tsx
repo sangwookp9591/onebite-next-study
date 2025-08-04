45aef089df189514c89872a0226ac338cf5ad35a
sangwookpark <sangwookp9591@gmail.com>
2025-08-04 18:44:00 +0900

    docs: query string에서 getStaticProps를 사용못하는 이유

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -3,6 +3,9 @@ import SearchableLayout from '@/components/searchable-layout';
 import fetchBooks from '@/lib/fetch-books';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 
+// 여기에는 getStaticProps를 사용할 수 없다 , context값이 없은 static은 build타임에 딱한번만 실행되는거임
+// 검색결과, 리스트 정렬구조 등, 사용자 인터렉션으로 통해지는것은 query String을 꺼내올 방법이 없음
+// 이러한 제약사항을 뚫고도하고싶으면 fetchBooks과정을 clinet쪽에서 해줘여함
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
     // qeury string을 받아오기 위해
     console.log('context : ', context); //현재 브라우저로부터 받은 모든 정보가 다포함이 되어있음.
