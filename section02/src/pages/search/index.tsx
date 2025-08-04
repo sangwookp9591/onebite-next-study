04c6df7c9cf43733b83447146fd06792a6e2d24e
sangwookpark <sangwookp9591@gmail.com>
2025-08-04 15:52:33 +0900

    feat : getServerSideProps를 추가하여 검색페이지 기능추가

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,19 +1,35 @@
+import BookItem from '@/components/book-item';
 import SearchableLayout from '@/components/searchable-layout';
-import { useRouter } from 'next/router'; //navigation은 app router에서 사용
+import fetchBooks from '@/lib/fetch-books';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 
-export default function Page() {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+    // qeury string을 받아오기 위해
+    console.log('context : ', context); //현재 브라우저로부터 받은 모든 정보가 다포함이 되어있음.
+    const q = context.query?.q;
+
+    const books = await fetchBooks(q as string);
+    return { props: { books } };
+};
+export default function Page({ books }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     //?q=asdasd
     //query string 가져오는 방법
 
-    const router = useRouter();
-    console.log('router : ', router);
+    // const router = useRouter();
+    // console.log('router : ', router);
 
-    const { q } = router.query;
+    // const { q } = router.query;
 
     //search?q=asdasd
     // 이렇게했을때 Log가 두번찍히는 이유는 querystring 읽는과정중 component를 한번더 렌더링시키기때문
 
-    return <h1>search {q}</h1>;
+    return (
+        <div>
+            {books.map((book) => (
+                <BookItem key={book?.id} {...book} />
+            ))}
+        </div>
+    );
 }
 
 Page.getLayout = (page: React.ReactNode) => {
