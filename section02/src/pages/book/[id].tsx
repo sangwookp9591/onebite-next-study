b72aa02465f73a1e00966182877a0d506762161b
sangwookpark <sangwookp9591@gmail.com>
2025-08-05 01:16:34 +0900

    refactor : fallback true - 빈 페이지를 먼저 내려주고 그다음 props를 다로 반환

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -45,7 +45,12 @@ export const getStaticPaths = () => {
          * fallback: 'blocking'
          * build time이후에 생성된 이런 페이지들은 처음 요청할 때에는 즉각적으로 생성이 되어야 되기 때문에 SSR방식으로 동작해서 비교적 느리기게 페이지가 렌더링 될 수 있지만 한 번만 만들면 넥스트 서버에 저장이 되기 때문에 그 이후 요청은 페이지를 새롭게 생성할 피요가 없다.
          */
-        fallback: 'blocking',
+
+        /**
+         * true로 하면 페이지는 즉각적으로 생성을하고 props(data)가 없는 빈 페이지를 반환하고 props를 계산해서 데이터가 있는 상태의 페이지를 렌더링함
+         *
+         */
+        fallback: true,
     };
 };
 
