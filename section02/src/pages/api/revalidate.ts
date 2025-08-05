6e4fc231142d8d67614e9d07811880689812d1e3
sangwookpark <sangwookp9591@gmail.com>
2025-08-05 21:20:32 +0900

    docs : revalidate함수 인지값 설명 추가

--- section02/src/pages/api/revalidate.ts
+++ section02/src/pages/api/revalidate.ts
@@ -5,7 +5,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         //index 페이지를 요청받았을때 revalidate ->재생성시킴
-        await res.revalidate('/');
+        await res.revalidate('/'); // 어떤경로를 revalidate 할것인지?
         return res.json({ revalidate: true }); //요청이 설공했다는 가정하에 revalidate : true를 반환함
     } catch (err) {
         console.log('revalidate err : ', err);
