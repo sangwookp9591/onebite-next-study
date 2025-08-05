d0fc1a63ba8266e5f401c05d06d0d6ce2c112cc7
sangwookpark <sangwookp9591@gmail.com>
2025-08-05 21:32:27 +0900

    feat : 검색 결과 페이지에 메타 테그 추가

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -2,6 +2,7 @@ import BookItem from '@/components/book-item';
 import SearchableLayout from '@/components/searchable-layout';
 import fetchBooks from '@/lib/fetch-books';
 import { BookData } from '@/types';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
@@ -32,12 +33,23 @@ export default function Page() {
             fetchData();
         }
     }, [q]);
+
     return (
-        <div>
-            {books.map((book) => (
-                <BookItem key={book?.id} {...book} />
-            ))}
-        </div>
+        <>
+            <Head>
+                <title>한입 북스 - 검색결과</title>
+                <meta property="og:image" content="/thumbnail.png" />
+                {/* og:image 로 썸네일을 설정할거라고 알림 / 는  project의 public 경로를 나타내는 거임 */}
+                <meta property="og:title" content="한입 북스 - 검색결과" />
+                <meta property="og:description" content="한입북스에 등록된 도서들을 만나보세요" />
+            </Head>
+
+            <div>
+                {books.map((book) => (
+                    <BookItem key={book?.id} {...book} />
+                ))}
+            </div>
+        </>
     );
 }
 
