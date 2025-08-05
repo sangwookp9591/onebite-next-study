a1afc1dc1af5671d7111b18774379955793b9d17
sangwookpark <sangwookp9591@gmail.com>
2025-08-05 21:13:37 +0900

    refactor: ISR기능 제거 기본적인 SSG페이지로 변경

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -38,7 +38,7 @@ export const getStaticProps = async () => {
             allBooks,
             recoBooks,
         },
-        revalidate: 3, //3초  주기로 재검증하겠다.
+        // revalidate: 3, //3초  주기로 재검증하겠다.
     };
 };
 
