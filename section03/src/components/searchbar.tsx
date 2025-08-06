e5b4a6cc86042e4739e28822718386b159c265fa
sangwookpark <sangwookp9591@gmail.com>
2025-08-06 19:27:01 +0900

    feat : searchpage 이동시 프로그래메틱하게 페이지 이동

--- section03/src/components/searchbar.tsx
+++ section03/src/components/searchbar.tsx
@@ -1,18 +1,27 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 //App route에서는 파일이름이 page나 layout이 아니면 일반적인 자바스크립트, 타입스크립트로 간주함.
 export default function Searchbar() {
+    const router = useRouter();
     const [search, setSearch] = useState('');
 
     const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
     };
+
+    const onSubmit = () => {
+        if (!search) {
+            return;
+        }
+        router.push(`/search?q=${search}`);
+    };
     return (
         <div>
             <input type="text" name="" id="" value={search} onChange={onChangeSearch} />
-            <button>검색</button>
+            <button onClick={onSubmit}>검색</button>
         </div>
     );
 }
