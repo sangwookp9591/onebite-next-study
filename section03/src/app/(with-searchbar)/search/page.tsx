e2f0d36b64be6cec58d878dd70ab27502baeb8bb
sangwookpark <sangwookp9591@gmail.com>
2025-08-06 19:13:41 +0900

    refactor : _  params와 searchParams는 타입상 Promise처럼 보일 수 있지만, 실제로는 await 하지 않아도 동기적으로 사용 가능합니다. -  params: Promise<...>로 나오는 건 Next.js 내부 구현이 lazy evaluation / async context를 사용해서 그렇지, 공식적으로 params는 Promise가 아닙니다.

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,15 @@
-export default async function Page({ searchParams }: { searchParams: Promise<{ q: string }> }) {
+import ClientComponent from '@/components/client-component';
+
+export default function Page({ searchParams }: { searchParams: { q?: string } }) {
     //App라우터에서 query string이나, url parameter와 같은 경로상에 포함되는 값들은 Page Component의 props로 받아온다
 
-    const { q } = await searchParams;
-    return <div>Search 페이지 {q}</div>;
+    // const { q } = await searchParams;
+    return (
+        <div>
+            Search 페이지 {searchParams?.q}
+            <ClientComponent>
+                <></>
+            </ClientComponent>
+        </div>
+    );
 }
--- section03/src/app/book/[id]/page.tsx
+++ section03/src/app/book/[id]/page.tsx
@@ -1,4 +1,3 @@
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
-    return <div>book/[{id}] page입니다.</div>;
+export default function Page({ params }: { params: { id: string | string[] } }) {
+    return <div>book/[id] page입니다. : {params?.id}</div>;
 }
