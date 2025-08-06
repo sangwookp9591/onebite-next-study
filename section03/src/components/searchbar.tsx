468a72d22f7b9eb34b003e8a3b6de4f12f5a351e
sangwookpark <sangwookp9591@gmail.com>
2025-08-06 19:42:34 +0900

    reafctor : keydown event추가 및 next/navigation을 이용한 qeury string존재 유무 파악

--- section03/src/components/searchbar.tsx
+++ section03/src/components/searchbar.tsx
@@ -1,27 +1,43 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import style from "./serachbar.module.css";
 
-//App route에서는 파일이름이 page나 layout이 아니면 일반적인 자바스크립트, 타입스크립트로 간주함.
 export default function Searchbar() {
-    const router = useRouter();
-    const [search, setSearch] = useState('');
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [search, setSearch] = useState("");
 
-    const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-    };
+  const q = searchParams.get("q");
 
-    const onSubmit = () => {
-        if (!search) {
-            return;
-        }
-        router.push(`/search?q=${search}`);
-    };
-    return (
-        <div>
-            <input type="text" name="" id="" value={search} onChange={onChangeSearch} />
-            <button onClick={onSubmit}>검색</button>
-        </div>
-    );
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div className={style.container}>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+      />
+      <button onClick={onSubmit}>검색</button>
+    </div>
+  );
 }
