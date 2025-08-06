cffd9c0926fd95eca839225518ef37fb3b7d5586
sangwookpark <sangwookp9591@gmail.com>
2025-08-06 21:51:06 +0900

    feat : server component data fetching

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -1,22 +1,25 @@
-import BookItem from "@/components/book-item";
-import style from "./page.module.css";
-import books from "@/mock/books.json";
+import BookItem from '@/components/book-item';
+import style from './page.module.css';
+import books from '@/mock/books.json';
 
-export default function Home() {
-  return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
-  );
+export default async function Home() {
+    const res = await fetch(`http://localhost:12345/book`);
+    const allBooks = await res.json();
+
+    return (
+        <div className={style.container}>
+            <section>
+                <h3>지금 추천하는 도서</h3>
+                {books.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </section>
+            <section>
+                <h3>등록된 모든 도서</h3>
+                {allBooks.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </section>
+        </div>
+    );
 }
