808ecfc5ffe05055f0838c3d0f285180a08ae115
sangwookpark <sangwookp9591@gmail.com>
2025-08-06 16:38:08 +0900

    feat : co-location App route에서는 파일이름이 page나 layout이 아니면 일반적인 자바스크립트, 타입스크립트로 간주함.

--- section03/src/app/(with-searchbar)/searchbar.tsx
+++ section03/src/app/(with-searchbar)/searchbar.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 
+//App route에서는 파일이름이 page나 layout이 아니면 일반적인 자바스크립트, 타입스크립트로 간주함.
 export default function Searchbar() {
     const [search, setSearch] = useState('');
 
