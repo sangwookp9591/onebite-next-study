c2531b55c091070a1873768ba42e648ae8230214
sangwookpark <sangwookp9591@gmail.com>
2025-08-06 14:20:39 +0900

    feat : url parameter 값 받아오기

--- section03/src/app/book/[id]/page.tsx
+++ section03/src/app/book/[id]/page.tsx
@@ -1,3 +1,4 @@
-export default function Page() {
-    return <div>book/[id] page입니다.</div>;
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    return <div>book/[{id}] page입니다.</div>;
 }
