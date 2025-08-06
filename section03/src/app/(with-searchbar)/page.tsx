62681d63565c62dfc1ec346c06e6bb1e70c9a33f
sangwookpark <sangwookp9591@gmail.com>
2025-08-06 19:25:05 +0900

    refactor : page 혼동방지를 위해 components 폴더로 이동

--- section03/src/app/(with-searchbar)/page.tsx
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,6 @@
 import ClientComponent from '../../components/client-component';
 import styles from './page.module.css';
-import ServerComponent from './server-component';
+import ServerComponent from '../../components/server-component';
 
 export default function Home() {
     console.log('Home 컴포넌트 실행'); // server component기 때문에 브라우저에서는 log가 남지 않는다.
