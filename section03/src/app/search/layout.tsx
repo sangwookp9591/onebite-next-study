84fb108ebdad78917fccbda1c9d603b5ffa2e766
sangwookpark <sangwookp9591@gmail.com>
2025-08-06 15:26:43 +0900

    feat : 렌더링할 페이지 컴포넌트 배치

--- section03/src/app/search/layout.tsx
+++ section03/src/app/search/layout.tsx
@@ -1,7 +1,10 @@
-export default function Layout() {
+import React from 'react';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div>
             <div>임시 서치바</div>
+            {children}
         </div>
     );
 }
