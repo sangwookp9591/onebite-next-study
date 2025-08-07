8e70609d650a4d1e463556f34b19c3a33fc869fb
sangwookpark <sangwookp9591@gmail.com>
2025-08-07 17:55:37 +0900

    feat : Cache테스트를 위해 Next App에서 발생하는 모든 데이터 패칭이 다 로그로써 자동으로 콘솔에 출력

--- section04/next.config.ts
+++ section04/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    logging: {
+        fetches: {
+            fullUrl: true, //Next App에서 발생하는 모든 데이터 패칭이 다 로그로써 자동으로 콘솔에 출력
+        },
+    },
 };
 
 export default nextConfig;
