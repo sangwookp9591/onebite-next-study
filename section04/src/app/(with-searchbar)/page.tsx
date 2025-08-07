095c411b6e46c0afa9f8ac3f800063bd6dec24ef
sangwookpark <sangwookp9591@gmail.com>
2025-08-07 16:28:56 +0900

    feat : Data패칭시  fetch 를 두번 사용해야할때는 가독성을 위해 컴포넌트를 나눠서 사용

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -1,24 +1,53 @@
 import BookItem from '@/components/book-item';
 import style from './page.module.css';
 import books from '@/mock/books.json';
+import { BookData } from '@/types';
+
+async function AllBooks() {
+    const res = await fetch(`http://localhost:12345/book`);
+    if (!res.ok) {
+        return <div>오류가 발생했습니다.</div>;
+    }
+    const allBooks: BookData[] = await res.json();
+
+    return (
+        <div>
+            {' '}
+            {allBooks.map((book) => (
+                <BookItem key={book.id} {...book} />
+            ))}
+        </div>
+    );
+}
+
+async function RecoBooks() {
+    const res = await fetch(`http://localhost:12345/book/random`);
+    if (!res.ok) {
+        return <div>오류가 발생했습니다.</div>;
+    }
+    const recoBooks: BookData[] = await res.json();
+    return (
+        <div>
+            {recoBooks.map((book) => (
+                <BookItem key={book.id} {...book} />
+            ))}
+        </div>
+    );
+}
 
 export default async function Home() {
-    const res = await fetch(`http://localhost:12345/book`);
-    const allBooks = await res.json();
+    // 이제 여기서 추천도서까지 가져 오려면 fetch 를 두번 사용해야하고 예외 처리를 두번해줘야하기때문에
+    // 내부코드가 길어져서 불러와야하는 데이터에따라서 컴포넌트를 나눠서 사용
 
     return (
         <div className={style.container}>
             <section>
                 <h3>지금 추천하는 도서</h3>
-                {books.map((book) => (
-                    <BookItem key={book.id} {...book} />
-                ))}
+                <RecoBooks />
             </section>
             <section>
                 <h3>등록된 모든 도서</h3>
-                {allBooks.map((book) => (
-                    <BookItem key={book.id} {...book} />
-                ))}
+                <AllBooks />
             </section>
         </div>
     );
