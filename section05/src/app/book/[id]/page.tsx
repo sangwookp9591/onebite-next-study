13a233373ab19d6d188da6e7227fc39d2874d531
sangwookpark <sangwookp9591@gmail.com>
2025-08-08 00:11:12 +0900

    refactor : generateStaticParams를 통해서 static page로 변경

--- section05/src/app/book/[id]/page.tsx
+++ section05/src/app/book/[id]/page.tsx
@@ -1,6 +1,19 @@
 import { BookData } from '@/types';
 import style from './page.module.css';
 
+/**
+ * 어떤 url parameter가 들어올지 모르는 동적경로를 받는 페이지라서
+ * dynamic page이다.
+ *
+ * Static page로 만들려면 어떤 경로가 존재할수 있는지 알려줘야함
+ * - 북페이지에서 어떤한 도서 데이터들이 빌드 타임에 만들어져야 되는지 먼저 알려주면 됨
+ * generateStaticParams 함수 생성
+ * generateStaticParams는 이름그대로 정적인 params를 생성하는 함수
+ */
+
+export function generateStaticParams() {
+    return [{ id: '1' }, { id: '2' }, { id: '3' }]; //build타임에 만들어줌
+}
 export default async function Page({ params }: { params: Promise<{ id: string | string[] }> }) {
     const { id } = await params;
 
