e1082817d57455f281241089583e075849c6ae85
sangwookpark <sangwookp9591@gmail.com>
2025-08-08 18:31:41 +0900

    feat : Error Handling

---
+++ section06/src/app/(with-searchbar)/error.tsx
@@ -0,0 +1,10 @@
+'use client';
+// client component라고 명시하는이유는 오류는 서버든 클라이언트든 발생할 수 있기 때문이다.
+//파일과 같은 경로에 있거나 또는 하위 경로에 있는 페이지에서 오류가 발생하면 에러 컴포넌트가 페이지 컴포넌트 대신에 화면에 노출
+export default function Error() {
+    return (
+        <div>
+            <h3>오류가 발생했습니다.</h3>
+        </div>
+    );
+}
