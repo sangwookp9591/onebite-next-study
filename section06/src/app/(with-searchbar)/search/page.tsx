5f3355981d50b73da084dcffb8f51faf7443f4fd
sangwookpark <sangwookp9591@gmail.com>
2025-08-08 15:13:10 +0900

    feat : Streaming을 delay걸이서 loading 컴포넌트가 대체 UI로서 동작하는지 확인

--- section06/src/app/(with-searchbar)/search/page.tsx
+++ section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,10 +1,13 @@
 import BookItem from '@/components/book-item';
 import { BookData } from '@/types';
+import { delay } from '@/util/delay';
 
 export default async function Page({ searchParams }: { searchParams: Promise<{ q?: string }> }) {
     const { q } = await searchParams;
 
     //검색이 늦어질경우 전체 페이지가 지체되지 않도록 설정
+
+    // await delay(3000);
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`, {
         cache: 'force-cache',
     });
