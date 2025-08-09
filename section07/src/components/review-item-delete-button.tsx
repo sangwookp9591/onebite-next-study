e0221f43ecfc382de564e5d00a3fd04d58939f7e
sangwookpark <sangwookp9591@gmail.com>
2025-08-09 21:22:06 +0900

    feat : useRef와  requestSubmit()을활용하여 div를 클릭하여도 요청되도록 수정

--- section07/src/components/review-item-delete-button.tsx
+++ section07/src/components/review-item-delete-button.tsx
@@ -4,12 +4,13 @@ import { deleteReviewAction } from '@/actions/delete-review.action';
 import { useActionState, useRef } from 'react';
 
 export default function ReviewItemDeleteButton({ bookId }: { bookId: number }) {
+    const formRef = useRef<HTMLFormElement>(null); //div를 button submit처럼 사용하기 위해
     const [state, formAction, isPending] = useActionState(deleteReviewAction, null);
 
     return (
-        <form action={formAction}>
+        <form action={formAction} ref={formRef}>
             <input name="bookId" type="text" value={bookId} hidden />
-            <div>{isPending ? '...' : '삭제하기'}</div>
+            <div onClick={() => formRef?.current?.requestSubmit()}>{isPending ? '...' : '삭제하기'}</div>
         </form>
     );
 }
