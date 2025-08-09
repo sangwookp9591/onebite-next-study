8b932f3a597b97059d7089b2fe68018083366fb3
sangwookpark <sangwookp9591@gmail.com>
2025-08-09 22:24:38 +0900

    feat : feed slot 부모 layout에 추가

--- section08/src/app/parallel/layout.tsx
+++ section08/src/app/parallel/layout.tsx
@@ -1,9 +1,18 @@
 import { ReactNode } from 'react';
 
-export default function Layout({ children, sidebar }: { children: ReactNode; sidebar: ReactNode }) {
+export default function Layout({
+    children,
+    sidebar,
+    feed,
+}: {
+    children: ReactNode;
+    sidebar: ReactNode;
+    feed: ReactNode;
+}) {
     return (
         <div>
             {sidebar}
+            {feed}
             {children}
         </div>
     );
