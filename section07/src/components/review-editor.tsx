00620b7c5cdb8b1f192ae90da14e5cceba556814
sangwookpark <sangwookp9591@gmail.com>
2025-08-09 20:25:33 +0900

    docs(7): 큰문제는 버튼 click후 form 중복 호출도 방지가 되어 있지 않는 이슈 발생

--- section07/src/components/review-editor.tsx
+++ section07/src/components/review-editor.tsx
@@ -1,6 +1,12 @@
+'use client';
+
 import styles from './review-editor.module.css';
 import { createReviewAction } from '@/actions/create-review.action';
 
+/**
+ * Server Action으로는 ,loading, button Disable등 어떤 기능도 제공되고 있지 않기때문에 답답할 수 있다.
+ * 큰문제는 버튼 click후 form 중복 호출도 방지가 되어 있지 않다.
+ */
 export default function ReviewEditor({ bookId }: { bookId: string }) {
     return (
         <section>
