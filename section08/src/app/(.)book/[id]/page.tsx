e9bf32ca4d84d13c310ee6d82d3e3682041c9797
sangwookpark <sangwookp9591@gmail.com>
2025-08-10 14:58:59 +0900

    Feat : Modal 추가

--- section08/src/app/(.)book/[id]/page.tsx
+++ section08/src/app/(.)book/[id]/page.tsx
@@ -1,11 +1,12 @@
 import BookPage from '@/app/book/[id]/page';
+import Modal from '@/components/modal';
 
 // Book Page 인터셉팅
 export default function Page(props: any) {
     return (
-        <div>
+        <Modal>
             특저 book page 가로채기 성공
             <BookPage {...props} />
-        </div>
+        </Modal>
     );
 }
