39e937f0a4d32b977299fd60059bd50819852169
sangwookpark <sangwookp9591@gmail.com>
2025-08-10 14:58:48 +0900

    feat : 외부 배경 클릭시 및 , ESC눌렀을때 뒤로가기 기능 추가

--- section08/src/components/modal.tsx
+++ section08/src/components/modal.tsx
@@ -3,22 +3,22 @@
 import { ReactNode, useEffect, useRef } from 'react';
 import styles from './modal.module.css';
 import { createPortal } from 'react-dom';
+import { useRouter } from 'next/navigation';
+//createPortal
+//첫번째 인수- modal의 렌더링 결과
+//두번째 인수- modal이 렌더링 될 위치인 DOM 요소
 
+//createPortal을 사용해서 만드는 이유
+//1.  DOM 계층 구조에서 독립적으로 동작하도록 하기 위해
+//2.  1. CSS z-index 및 레이아웃 제약 해소 모달을 부모 컴포넌트 안에서 렌더링하면, 부모의 overflow: hidden, z-index 값, position 등에 영향을 받아서 화면 전체에 제대로 떠야 하는 모달이 잘리거나 가려질 수 있어요.
+// createPortal을 사용하면 <body>나 별도 의 상위 DOM 노드에 모달을 직접 렌더링해서 이런 스타일 제약을 피할 수 있습니다.
+
+//전체화면을 다 뒤덮는 그러한 글로벌한 요소들이 될탠데 특정 페이지 컴포넌트에서 보면<div></div>안에 들어가 있으면 어색하기때문에
+//dom요소안에 modal-root에 고정적으로 모달이 위치하게끔 설정ㄴ
 export default function Modal({ children }: { children: ReactNode }) {
-    //createPortal
-    //첫번째 인수- modal의 렌더링 결과
-    //두번째 인수- modal이 렌더링 될 위치인 DOM 요소
-
-    //createPortal을 사용해서 만드는 이유
-    //1.  DOM 계층 구조에서 독립적으로 동작하도록 하기 위해
-    //2.  1. CSS z-index 및 레이아웃 제약 해소 모달을 부모 컴포넌트 안에서 렌더링하면, 부모의 overflow: hidden, z-index 값, position 등에 영향을 받아서 화면 전체에 제대로 떠야 하는 모달이 잘리거나 가려질 수 있어요.
-    // createPortal을 사용하면 <body>나 별도 의 상위 DOM 노드에 모달을 직접 렌더링해서 이런 스타일 제약을 피할 수 있습니다.
-
-    //전체화면을 다 뒤덮는 그러한 글로벌한 요소들이 될탠데 특정 페이지 컴포넌트에서 보면<div></div>안에 들어가 있으면 어색하기때문에
-    //dom요소안에 modal-root에 고정적으로 모달이 위치하게끔 설정
-
     //dialog tag는 기본적으로 modal의 역할을 하기때문에 처음 렌더링되었을때 modal이 꺼져있는 상태이다.
     const dialogRef = useRef<HTMLDialogElement>(null);
+    const router = useRouter();
 
     //modal component가 처음 마운트 되었을때
     useEffect(() => {
@@ -31,7 +31,19 @@ export default function Modal({ children }: { children: ReactNode }) {
         }
     }, []);
     return createPortal(
-        <dialog className={styles.modal} ref={dialogRef}>
+        <dialog
+            className={styles.modal}
+            ref={dialogRef}
+            onClick={(e) => {
+                //모달의 배경이 클리이된거면 -> 뒤로가기
+                if ((e.target as any).nodeName === 'DIALOG') {
+                    //DIALOG이면 외부를 클릭한거임
+                    router.back();
+                }
+            }}
+            onClose={() => router.back()}
+            // ESC눌렀을때도 뒤로가기
+        >
             {children}
         </dialog>,
         document.getElementById('modal-root') as HTMLElement
