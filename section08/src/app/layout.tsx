07ac3cf79e2539de71d78c28a85b5dfd8b3c46ff
sangwookpark <sangwookp9591@gmail.com>
2025-08-10 14:48:59 +0900

    feat :createPortal과 연결하기 위해 그리고css제약을 피하기 위해 body 밑에 modal-root div생성

--- section08/src/app/layout.tsx
+++ section08/src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
                     <Footer />
                 </div>
             </body>
+            <div id="modal-root"></div>
         </html>
     );
 }
