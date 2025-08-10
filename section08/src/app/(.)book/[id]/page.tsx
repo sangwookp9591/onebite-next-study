c563b60eff4e5fcc18ec9d5e82543a86f88b3b75
sangwookpark <sangwookp9591@gmail.com>
2025-08-10 14:40:00 +0900

    feat : book page를 그대로 보여주기위해 Import

--- section08/src/app/(.)book/[id]/page.tsx
+++ section08/src/app/(.)book/[id]/page.tsx
@@ -1,4 +1,11 @@
+import BookPage from '@/app/book/[id]/page';
+
 // Book Page 인터셉팅
-export default function Page() {
-    return <div>특저 book page 가로채기 성공</div>;
+export default function Page(props: any) {
+    return (
+        <div>
+            특저 book page 가로채기 성공
+            <BookPage {...props} />
+        </div>
+    );
 }
