b1822812a3a21acda29f9e2e81e3d05840190c47
sangwookpark <sangwookp9591@gmail.com>
2025-08-10 18:03:52 +0900

    feat : 동적 메타데이터 등록

--- section09/src/app/(with-searchbar)/search/page.tsx
+++ section09/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,7 @@
 import BookItem from '@/components/book-item';
 import { BookData } from '@/types';
 import { delay } from '@/util/delay';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
 
 //비동기 부분을 Component로 분리
@@ -23,6 +24,21 @@ async function SearchResult({ q }: { q?: string }) {
     );
 }
 
+export async function generateMetadata({ searchParams }: { searchParams: Promise<{ q?: string }> }): Promise<Metadata> {
+    //현재 페이지 메타 데이터를 동적으로 생성하는 역할을 한다.
+    //페이지 컴포넌트가 전달받는 페이지의 매개변수도 받을 수있다.
+
+    const { q } = await searchParams;
+    return {
+        title: `${q} : 한입북스 검색`,
+        description: `${q}의 검색 결과입니다 `,
+        openGraph: {
+            title: `${q} : 한입북스 검색`,
+            description: `${q}의 검색 결과입니다 `,
+            images: ['/thumbnail.png'], //썸네일 /->는 Pulbic폴더를 가르킨다.
+        },
+    };
+}
 // 15.1버전부터는 async를 붙여야함 searchParams와 params는 Promise객체를 붙여줘야한다.
 export default async function Page({ searchParams }: { searchParams: Promise<{ q?: string }> }) {
     const { q } = await searchParams;
