1a19d720b8143ad0a0765952470d4a2a37b987fa
sangwookpark <sangwookp9591@gmail.com>
2025-08-10 16:23:26 +0900

    feat :  domains: ['shopping-phinf.pstatic.net'], //이미지를 불러 올라고하는 서버의 도메인 추가

--- section09/next.config.ts
+++ section09/next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
             fullUrl: true, //Next App에서 발생하는 모든 데이터 패칭이 다 로그로써 자동으로 콘솔에 출력
         },
     },
+    images: {
+        domains: ['shopping-phinf.pstatic.net'], //이미지를 불러 올라고하는 서버의 도메인 추가
+    },
 };
 
 export default nextConfig;
